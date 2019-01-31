a8a3fa78bc882d21d1b1855fe6e38bde4d9f84af
damiadeh <damiadehjames@gmail.com>
2019-01-31 09:06:38 +0100

    working on contact page

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -8,9 +8,9 @@ class Modal extends Component{
     shouldComponentUpdate(nextProps, nextState){
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
-    componentWillUpdate(){
-        console.log('Modal updated')
-    }
+    // componentWillUpdate(){
+    //     console.log('Modal updated')
+    // }
     render () {
         return (
             <Aux>
--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -1,19 +1,27 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from '../containers/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: {}
+        ingredients: null,
+        totalPrice: 0
     }
 
-    componentDidMount() {
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()){
             //['salad', '1]//the plus added coverts it to a number
-            ingredients[param[0]] = +param[1];
+            if(param[0] ==='price'){
+                price = +param[1];
+            }else{
+                ingredients[param[0]] = +param[1];
+            }
         }
-        this.setState({ingredients: ingredients});
+        this.setState({ingredients: ingredients, totalPrice: price});
     }
 
     CheckoutCancelledHandler= () => {
@@ -31,6 +39,8 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     CheckoutCancelled={this.CheckoutCancelledHandler}
                     CheckoutContinued={this.CheckoutContinuedHandler} />
+                    {/* instead of using component we use render to be able to pass the ingredient data to the condata data page */}
+                    <Route path={this.props.match.path + '/contact-data'} render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
             </div>
         );
     }
