1fd9b61faa69efeabbe398772a47fe3380a9ccd5
damiadeh <damiadehjames@gmail.com>
2019-01-31 09:41:22 +0100

    Updates

---
+++ precache-manifest.024a4c7a2d69ff71e816d62e45e4651a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54235b09e2e8ed737c9a424648cd36b6",
+    "url": "/burgerbuilder/static/media/chef-burger.54235b09.jpg"
+  },
+  {
+    "revision": "9d7ef0e7a960638f7ec5",
+    "url": "/burgerbuilder/static/js/runtime~main.9d7ef0e7.js"
+  },
+  {
+    "revision": "c64ae97abc29e9d138d1",
+    "url": "/burgerbuilder/static/js/main.c64ae97a.chunk.js"
+  },
+  {
+    "revision": "32625b18c773c7f4b2b3",
+    "url": "/burgerbuilder/static/js/1.32625b18.chunk.js"
+  },
+  {
+    "revision": "c64ae97abc29e9d138d1",
+    "url": "/burgerbuilder/static/css/main.90b5a765.chunk.css"
+  },
+  {
+    "revision": "2b749577c53d0fa94d8de80d7671b349",
+    "url": "/burgerbuilder/index.html"
+  }
+];
