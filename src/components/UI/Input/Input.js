8d75bd2c50c3914ebce328ab8f58fd84d6bf2fa4
damiadeh <damiadehjames@gmail.com>
2019-02-05 14:37:49 +0100

    submitting customer data

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,15 +4,26 @@ import './Input.css'
 const input = (props) => {
     let inputElement = null;
 
-    switch(props.inputtype){
+    switch(props.elementType){
         case ('input'):
-            inputElement = <input className="InputElement" {...props} />;
+            inputElement = <input className="InputElement" {...props.elementConfig} value={props.value}  onChange={props.changed}/>;
             break;
         case('textarea'):
-            inputElement = <textarea className="InputElement" {...props} />;
+            inputElement = <textarea className="InputElement" {...props.elementConfig} value={props.value}  onChange={props.changed}/>;
+            break;
+        case('select'):
+            inputElement = (
+                <select 
+                    className="InputElement" 
+                    value={props.value} onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>{option.displayValue}</option>
+                    ))}
+                </select>
+            ); 
             break;
         default:
-            inputElement = <input className="InputElement" {...props} />;
+            inputElement = <input className="InputElement" {...props.elementConfig} value={props.value}  onChange={props.changed}/>;
     }
     return(
         <div className="Input">
