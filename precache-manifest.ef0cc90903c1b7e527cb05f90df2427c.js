5e9ab1702cd72478fc55e75841b8ea2e7ad30571
damiadeh <damiadehjames@gmail.com>
2019-03-07 17:29:18 +0100

    Updates

---
+++ precache-manifest.ef0cc90903c1b7e527cb05f90df2427c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54235b09e2e8ed737c9a424648cd36b6",
+    "url": "/damiburger/static/media/chef-burger.54235b09.jpg"
+  },
+  {
+    "revision": "2a9455aecba5e996ed81",
+    "url": "/damiburger/static/js/runtime~main.2a9455ae.js"
+  },
+  {
+    "revision": "108532dff3273d59c19c",
+    "url": "/damiburger/static/js/main.108532df.chunk.js"
+  },
+  {
+    "revision": "c783a0c43354327f907a",
+    "url": "/damiburger/static/js/1.c783a0c4.chunk.js"
+  },
+  {
+    "revision": "108532dff3273d59c19c",
+    "url": "/damiburger/static/css/main.500b6df1.chunk.css"
+  },
+  {
+    "revision": "f685766b11833a895a4ed31db7d8b9c1",
+    "url": "/damiburger/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/darger/precache-manifest.05bb67d902454a7a3aadcd1d3ff5789b.js"
+  "/damiburger/precache-manifest.ef0cc90903c1b7e527cb05f90df2427c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/darger/index.html", {
+workbox.routing.registerNavigationRoute("/damiburger/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
